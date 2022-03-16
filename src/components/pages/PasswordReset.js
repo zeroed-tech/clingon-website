8f8899e80338452724315747b2cf62d932da4c9c
Damian Justice <damian.justice@globalx.com.au>
2022-03-16 22:02:07 +1100

    security update

--- src/components/pages/PasswordReset.js
+++ src/components/pages/PasswordReset.js
@@ -16,53 +16,43 @@ import {
   } from "react-router-dom";
 import '../../css/general.css'
 
-export default function PasswordReset({ handleLogin }) {
+export default function PasswordReset() {
     let [oldPassword, setOldPassword] = useState('')
-    let [userName, setUserName] = useState('')
     let [newPassword, setNewPassword] = useState('')
     let [error, setError] = useState(false)
     let history = useHistory();
 
     let handleSubmit = () => {
-        fetch('https://clingonaustralia.com.au/login', {
+        fetch('https://clingonaustralia.com.au/passwordreset', {
             method: 'POST',
             header: {
                 'Content-Type': 'application/json'
             },
             credentials: 'include',
             body: JSON.stringify({
-                'username': userName,
-                'password': oldPassword
+                'CurrentPassword': oldPassword,
+                'NewPassword': newPassword,
+
             })
         })
             .then(resp => resp.json())
-            .then(data => {
-                data.success ? successfulLogin() : setError(true)
-                data.success ? handleLogin(userName) : setError(true)
-            
-            })
+            .then(data => successfulLogin())
             .catch(err => setError(true))
     }
     function successfulLogin() {
-        history.push("/store")
+        history.push("/login")
     }
     return (
         <Container>
             <Row className="justify-content-md-center">
                 <div id='login-container'>
-                    <h3>Stockist Login</h3>
+                    <h3>Stockist Password Reset</h3>
                     {error && <Alert className='alert' variant='danger'>
                         Username or password were incorrect, please try again.
                     </Alert>}
                     <Col sm={{ span: 4, offset: 4 }}>
 
                         <Form>
-
-                            <Form.Group className="mb-3" controlId="formPlaintextEmail">
-                                <Form.Label>Username</Form.Label>
-                                <Form.Control onChange={(e) => setUserName(e.target.value)} type="text" placeholder="Username" />
-                            </Form.Group>
-
                             <Form.Group className="mb-3" controlId="formPlaintextPassword">
                                 <Form.Label>Old password</Form.Label>
                                 <Form.Control onChange={(e) => setOldPassword(e.target.value)} type="password" placeholder="Old Password" />
